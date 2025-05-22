572dd9893e49af2f0c5ea6d86942e86356a0d3b7
Akash Gupta <acashgupta960@gmail.com>
2025-05-22 23:57:37 +0530

    night commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 As i Surfed the Whole Internet to find a Best Refrence or Source to Learn i Couldn't able to find any 
 
 So Decided to Create this Whole Free learner Resouce to Master Framer Motion My Refrence is Mighty Motion Guide which i bought for 800 rupees yearly subscription
+
+Chap 1 You Will found the detail Chapter 1 on our main page.tsx 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
+"use client";
 import Image from "next/image";
 
-export default function Home() {
+//// Most basic and Important import almost needed to created all animations
+import {motion} from "framer-motion"
+
+import React from 'react'
+
+const page = () => {
   return (
-   
-   <div>
-    <h1>Home</h1>
-   </div>
-  );
+    <div>
+      <h1>Hello </h1>
+
+       {/* This 100 Percent Rotation Property is called as Tween which just rotates When you add teh duration its the time it takes to complete duration longer the duration slower the time to 
+       rotate One very cool property is repeat Infinity it literaaly makes your div or elemnet revolve infinitely */}
+        <motion.div
+            style={{
+                width: 150,
+                height: 150,
+                borderRadius: 50,
+                backgroundColor: "#fff",
+                border: "2px solid black"
+            }}
+            animate={{ rotate: 360 }}
+            transition={{ duration: 1, repeat: Infinity }}
+        >
+
+        
+          </motion.div>
+      
+    </div>
+  )
 }
+
+export default page
+
